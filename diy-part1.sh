d5c68a7bb191996280906f0c045de60031651c35
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-05-15 15:57:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,25 +13,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
-git clone https://github.com/skyformat99/eqos.git package/eqos
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/frainzy1477/luci-app-clash.git package/clash
-git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/unblockneteasemusicadvance
-git clone https://github.com/Sunnice/openwrt-package.git package/lienol
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonnew
-git clone https://github.com/Lienol/luci-theme-darkmatter.git package/luci-theme-darkmatter
-git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
-git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/luci-theme-atmaterial
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomato
-git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly.git package/luci-theme-Butterfly
-git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark.git package/luci-theme-Butterfly-dark
-git clone https://github.com/Leo-Jo-My/luci-theme-leo.git package/luci-theme-leo
-git clone https://github.com/rosywrt/luci-theme-purple.git package/luci-theme-purple
-git clone https://github.com/Immueggpain/luci-theme-linksysmaterial.git package/luci-theme-linksysmaterial
+
